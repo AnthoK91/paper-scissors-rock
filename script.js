d0375db5d2b4e0399e8b5b65f5d4d4b4e6f71f8e
Anthony Kirkham <antho.kirkham@gmail.com>
2023-11-09 20:35:15 +1000

    added computer choice

---
+++ script.js
@@ -0,0 +1,13 @@
+const choices = ["rock", "paper", "scissors"]
+const random = Math.floor(Math.random() * choices.length)
+
+function getComputerChoice() {
+return choices[random];
+}
+
+console.log(getComputerChoice());
+
+function playRound (playerselection, computerSelection) {
+
+}
+
